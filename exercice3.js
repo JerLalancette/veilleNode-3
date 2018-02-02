f9c7068f6c728a4867c278f64e5303b12afcee33
JerLalancette <lalancette.jerermie@gmail.com>
2018-02-02 15:57:03 -0500

    tableau json provinces

--- exercice3.js
+++ exercice3.js
@@ -1,11 +1,44 @@
-"use strict";
-/* importation du module «fs» (file system) */
-let fs = require("fs");
+const http = require("http"); 
+const fs = require("fs");
 
-/* lecture synchrone, la fonction: fs.readFileSync() 
-bloque l'éxécution des instructions suivantes */
+let provinces;
 
-let data = fs.readFileSync('03_fichier.txt');
+fs.readFile('provinces.json', 'utf8', (err, data) => {
+    if (err) throw(err);
+    provinces = JSON.parse(data);
+}) ;
 
-console.log(data.toString());
-console.log("Fin du programme");
+http.createServer((request, response) => 
+{ 
+
+
+    /* La fonction fs.readFile() est asynchrone elle ne bloque pas l'éxécution des instructions suivantes */
+    
+    console.log(provinces);
+
+    let html = "<html>";
+    html += "<style> td{border: 1px solid red;}";
+    html += "</style>";
+    html += "<thead>";
+    html += "les provinces";
+    html += "</thead>";
+
+    html += "<table>";
+    for (let province in provinces) {
+        html += "<tr>";
+        html += "<td>";
+        html += provinces[province];
+        html += "</td>";
+        html += "<td>";
+        html += province;
+        html += "</td>";
+        html += "</tr>";
+    }
+    html += "</table>";
+    html += "</html>"
+
+    response.writeHead(200, {"Content-Type": "text/html; Charset=UTF-8"});
+    response.write(html); 
+    response.end();
+
+}).listen(3000);
